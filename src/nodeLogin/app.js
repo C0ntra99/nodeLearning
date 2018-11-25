f77f319dd379e1eae2f7bae27f5d32edefacc4e5
C0ntra99 <fowlerethan99@gmail.com>
2018-11-24 20:47:09 -0600

    added sql authentication to the login script, also added a signup page. Implemented error printing and changed to sessions

--- src/nodeLogin/app.js
+++ src/nodeLogin/app.js
@@ -3,20 +3,33 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const cookieParser = require('cookie-Parser');
+const session = require('express-session');
+const sqlite3 = require('sqlite3');
+
+var initDB = (db) => {
+    var db = new sqlite3.Database(db, (err) => {
+        if (err) throw err;
+    });
+    return db;
+}
 
 //App depencies 
-app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session({secret:'SecretID'}));
 
+app.engine('pug', require('pug').__express);
 app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res, next) => {
-    if (typeof(req.cookies.userData) == 'undefined') {
+    if (!req.session.uname) {
         res.redirect('/login');
-    } else if (req.cookies.userData.loggedIn) {
-        res.sendFile(path.join(__dirname+'/views/index.html'));
+    } else if (req.session.loggedIn) {
+        res.render('index.pug', {username:req.session.uname});
+        console.log(req.session);
     } else {
         res.redirect('/login');
     } 
@@ -24,30 +37,57 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/login', (req, res, next) => {
-    res.sendFile(path.join(__dirname+'/views/login.html'));
+    res.render('login.pug');
 });
 
 app.post('/login', (req, res, next) => {
+    //init the database
+    let db = initDB('main.db');
+    let q = 'SELECT username, password FROM users WHERE username = ? AND password = ?'
     
-    var uname = req.body.uname;
-    if (uname == 'username' && req.body.passwd == 'password') {
-        
-        let user = {
-            name:req.body.uname,
-            loggedIn:true
+    db.get(q, [req.body.uname, req.body.passwd], (err, row) => {
+        if (err) throw err;
+        if (row) {
+            req.session.uname = req.body.uname;
+            req.session.loggedIn = true;
+            res.redirect('/'); 
+        } else {
+            res.render('login.pug', {err:"Username or password is incorrect"});
         }
-
-        res.cookie("userData", user);
-        res.redirect('/');
-    } else {
-        res.redirect('/login');
-        res.end();
-    }
+    });
+    
 });
 
 app.get('/logout', (req,res,next) => {
-    res.clearCookie('userData');
+    req.session.destroy( (err) => {
+        if (err) {
+            res.negotiate(err);
+        }
+    });
     res.redirect('/');
+});
+
+app.get('/signup', (req, res, next) => {
+    res.render('signup.pug');
+});
+
+app.post('/signup', (req, res, next) => {
+    let db = initDB('main.db');
+    let q = 'SELECT username FROM users WHERE username = ?';
+    
+    db.get(q, [req.body.uname], (err, row) => {
+        if (err) throw err;
+        if (row) {
+            res.render('signup.pug',{err:"Username already exist!"});
+        } else {
+            let q = 'INSERT INTO users (username, password) VALUES (?, ?)';
+            db.run(q,[req.body.uname, req.body.passwd], (err) => {
+                if (err) throw err;
+            });
+            res.redirect('/login');
+            res.end();
+        }
+    });
 });
 
 module.exports = app;
